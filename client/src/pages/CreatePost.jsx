9f9e2b8e88e3c299a7caea48b60ef703c1d0f2ff
berserk-healer <dandereperson@gmail.com>
2023-02-24 13:40:06 +0700

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -48,7 +48,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', 
+        const response = await fetch('https://153580.bellcranel.click/api/v1/post', 
         {
           method: 'POST',
           headers: {
